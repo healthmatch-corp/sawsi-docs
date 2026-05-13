b483d1118a781f26e2c3ec1f171b310780fb72ad
syc-weapon <syc@healthmatch.app>
2026-05-13 17:54:16 +0900

    feat: update shadcn theme and fonts

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,14 +1,10 @@
-import { RootProvider } from 'fumadocs-ui/provider/next';
-import '@/app/global.css';
-import { Inter } from 'next/font/google';
-import { i18n } from '@/lib/i18n';
-import { i18nUI } from '@/lib/layout.shared';
-import { getDictionary } from '@/dictionaries';
-import { DictionaryProvider } from '@/components/dictionary-provider';
+import { RootProvider } from "fumadocs-ui/provider/next";
+import "@/app/global.css";
 
-const inter = Inter({
-  subsets: ['latin'],
-});
+import { i18n } from "@/lib/i18n";
+import { i18nUI } from "@/lib/layout.shared";
+import { getDictionary } from "@/dictionaries";
+import { DictionaryProvider } from "@/components/dictionary-provider";
 
 export function generateStaticParams() {
   return i18n.languages.map((lang) => ({ lang }));
@@ -17,12 +13,12 @@ export function generateStaticParams() {
 export default async function Layout({
   params,
   children,
-}: LayoutProps<'/[lang]'>) {
+}: LayoutProps<"/[lang]">) {
   const { lang } = await params;
   const dictionary = await getDictionary(lang);
 
   return (
-    <html lang={lang} className={inter.className} suppressHydrationWarning>
+    <html lang={lang} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider i18n={i18nUI.provider(lang)}>
           <DictionaryProvider value={dictionary}>{children}</DictionaryProvider>
