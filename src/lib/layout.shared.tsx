e75c1dc25778a4b3cfb503855becb0c11efe3357
syc-weapon <syc@healthmatch.app>
2026-05-13 20:49:48 +0900

    feat: add logo files and apply logo component

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -1,22 +1,23 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
-import { defineI18nUI } from 'fumadocs-ui/i18n';
-import { i18n } from '@/lib/i18n';
-import { appName, gitConfig } from './shared';
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import { defineI18nUI } from "fumadocs-ui/i18n";
+import { i18n } from "@/lib/i18n";
+import { appName, gitConfig } from "./shared";
+import Image from "next/image";
 
 export const i18nUI = defineI18nUI(i18n, {
   en: {
-    displayName: 'English',
+    displayName: "English",
   },
   ko: {
-    displayName: '한국어',
-    search: '문서 검색',
-    toc: '목차',
-    chooseTheme: '테마 선택',
-    chooseLanguage: '언어 선택',
-    nextPage: '다음',
-    previousPage: '이전',
-    lastUpdate: '최종 수정',
-    editOnGithub: 'GitHub에서 편집',
+    displayName: "한국어",
+    search: "문서 검색",
+    toc: "목차",
+    chooseTheme: "테마 선택",
+    chooseLanguage: "언어 선택",
+    nextPage: "다음",
+    previousPage: "이전",
+    lastUpdate: "최종 수정",
+    editOnGithub: "GitHub에서 편집",
   },
 });
 
@@ -33,9 +34,21 @@ export function baseOptions(lang: string): BaseLayoutProps {
 
 function Logo() {
   return (
-    <div className="flex items-center gap-2">
-      <div className="size-6 rounded bg-gradient-to-br from-fd-primary to-fd-primary/40" />
-      <span className="font-semibold tracking-tight">{appName}</span>
-    </div>
+    <>
+      <Image
+        src="/logo-glyph-light.svg"
+        width={171}
+        height={50}
+        alt="sawsi logo"
+        className="dark:hidden aspect-171/50 w-[78px]"
+      />
+      <Image
+        src="/logo-glyph-dark.svg"
+        width={171}
+        height={50}
+        alt="sawsi logo"
+        className="hidden dark:inline-flex aspect-171/50 w-[78px]"
+      />
+    </>
   );
 }
